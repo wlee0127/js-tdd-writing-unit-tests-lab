63fb86aff4723126815426d0a33c1bd3433f520b
william lee <will.lee.npc@gmail.com>
2024-02-03 00:06:53 -0500

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,45 @@
 // Your tests here
+import {isPalindrome} from "../utils.js";
+
+describe("isPalindrome", ()=>{
+    it("should return true if word is a palindrome(reads the same forward and backward)", ()=>{
+        const testCase="mom";
+        
+        const result=isPalindrome(testCase);
+        
+        expect(result).toBe(true);
+    });
+
+    it("should return true if word is a palindrome & have mixed cases", ()=>{
+        const testCase="Mom";
+        
+        const result=isPalindrome(testCase);
+        
+        expect(result).toBe(true);
+    });
+
+    
+    it("should return false if word is an empty string", ()=>{
+        const testCase="";
+        
+        const result=isPalindrome(testCase);
+        
+        expect(result).toBe(false);
+    });
+
+    it("should throw an error if word contains non alphabetic chars", ()=>{
+        const testCase="wrt2";
+        
+        //const result=isPalindrome(testCase);
+        
+        expect(()=>{isPalindrome(testCase)}).toThrow();
+    });
+
+    it("should throw an error if input is not string", ()=>{
+        const testCase=[];
+        
+        //const result=isPalindrome(testCase);
+        
+        expect(()=>{isPalindrome(testCase)}).toThrow();
+    });
+} )
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome(arg) {
+    const cleanString = arg.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    if(arg.length===0) {
+        return false
+    }else if( !/^[A-Za-z]+$/.test(arg)){
+        throw new Error("word contains non-alphabateic chars");
+    }else if(typeof(arg)!=="string"){
+        throw new Error("input is not string");
+    }else {
+        return cleanString === cleanString.split('').reverse().join('')        
+    }
+}
